06dedaab1a69f22d1f00e7a5a87b437da5cd6874
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-06-05 22:17:30 +0000

    Formule section added to sidebar and menu btn hide for desktop

--- resources/views/components/app-navbar.blade.php
+++ resources/views/components/app-navbar.blade.php
@@ -3,7 +3,7 @@
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+            <a class="nav-link d-md-none" data-widget="pushmenu" href="#" role="button">
                 <i class="fas fa-bars"></i>
             </a>
         </li>
