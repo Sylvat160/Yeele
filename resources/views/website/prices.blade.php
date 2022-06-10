e0e52f9dca43eb730204dcbffc4958295c3d2ab3
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-06-10 11:56:46 +0000

    Live chat remove from prices features

--- resources/views/website/prices.blade.php
+++ resources/views/website/prices.blade.php
@@ -82,16 +82,6 @@
                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                                     clip-rule="evenodd"></path>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-300">Live chat</span>
-                        </li>
-                        <li class="flex space-x-3 line-through decoration-gray-300">
-                
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-300" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
                             <span class="text-base font-normal leading-tight text-gray-300">Customisation</span>
                         </li>
                         <li class="flex space-x-3 line-through decoration-gray-300">
@@ -161,16 +151,6 @@
                                     clip-rule="evenodd"></path>
                             </svg>
                             <span class="text-base font-normal leading-tight text-gray-500">Espace collaboratif</span>
-                        </li>
-                        <li class="flex space-x-3">
-                
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500">Live chat</span>
                         </li>
                         <li class="flex space-x-3 line-through decoration-gray-300">
                 
@@ -249,16 +229,6 @@
                                     clip-rule="evenodd"></path>
                             </svg>
                             <span class="text-base font-normal leading-tight text-yellow-400">Espace collaboratif</span>
-                        </li>
-                        <li class="flex space-x-3">
-                
-                            <svg class="flex-shrink-0 w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <span class="text-base font-normal leading-tight text-yellow-400">Live chat</span>
                         </li>
                         <li class="flex space-x-3">
                 
