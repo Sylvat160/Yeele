e3427c7f42857549758dc2e264ec0d43001e12e9
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-06-25 11:45:03 +0000

    Civility related error resolved

--- resources/views/forms/registration_form.blade.php
+++ resources/views/forms/registration_form.blade.php
@@ -49,8 +49,8 @@
                         class="bg-gray-50 outline-none transition-colors border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-1 focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
                         required>
                         <option class="hidden" selected>Selectionner votre civilité</option>
-                        <option value="Masculin">Monsieur</option>
-                        <option value="Feminin">Madame</option>
+                        <option value="Monsieur">Monsieur</option>
+                        <option value="Madame">Madame</option>
                         <option value="Mademoiselle">Mademoiselle</option>
                     </select>
                     @error('civility')
