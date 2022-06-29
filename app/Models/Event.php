fd92659f2ebcb6af5f5a557d114bb62b3e5508b4
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-06-29 14:38:46 +0000

    Event relation with user added

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -52,6 +52,10 @@ class Event extends Model
         });
     }
 
+    public function user() {
+        return $this->belongsTo(User::class, 'user_uid');
+    }
+
     public function category() {
         return $this->belongsTo(Category::class, 'category_uid');
     }
