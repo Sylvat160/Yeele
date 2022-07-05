e7b02c5f282f63ee834d209b4316d0cb2315accb
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-07-05 15:35:56 +0000

    Change

--- resources/views/app/edit-user-info.blade.php
+++ resources/views/app/edit-user-info.blade.php
@@ -3,6 +3,11 @@
 @section('bigTitle') Modification des informations @endsection
 
 @section('main')
+    {{-- STATUS --}}
+    @include('extras.success_message')
+    @include('extras.danger_message')
+    {{-- ENDSTATUS --}}
+
     <div class="row justify-content-center">
         <div class="col-12 col-md-10">
             
