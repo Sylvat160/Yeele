1384283bc87fbc9ba172a3171899e7ea8d95e70b
Aboubakary833 <aboubakarycisse472@gmail.com>
2022-07-23 15:32:57 +0000

    (New): 404 page for not found event addedd

--- resources/views/app/event_not_found.blade.php
+++ resources/views/app/event_not_found.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Yeele | Inscription réussi</title>
+    <title>Yeele | Evènement non trouvé!</title>
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
